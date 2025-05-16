99fb5906a21f79bf9c5ac46d07f3bf6a740ac225
Owen Kaluza <owen@kaluza.id.au>
2025-05-16 16:24:17 +1000

    Set timezone when freezing time for test

--- tests/runner.py
+++ tests/runner.py
@@ -7,7 +7,7 @@ if len(script):
     #Enable test mode
     import lavavu
     lavavu.settings['test_mode'] = True #Force testing mode
-    with freeze_time("2025-01-01 12:00AM"):
+    with freeze_time("2025-01-01 12:00AM GMT"):
         exec(open(script).read(), globals(), locals())
 else:
     print('Requires script to run as first argument')
